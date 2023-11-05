544846cd9f0c784134b7be26a561930a23e5d23b
Luis Orjuela <lhorjuelag@gmail.com>
2023-11-05 10:33:53 -0500

    Se inicializa el proyecto de angular

---
+++ aroma-magico/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CafesModule } from './cafes/cafes.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CafesModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
